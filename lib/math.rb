416b2b568109751dd5ebda27afa1b409b14060e6
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-06-26 14:03:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  result = num1 - num2
+  return result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  return result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  return result
 end
 
 def modulo(num1, num2)
